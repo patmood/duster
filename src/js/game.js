64b640645aee66e8b06dc75a74cf24dfba52545b
patmood <patrick.n.moody@gmail.com>
2014-11-23 16:03:12 -0800

    separate plane behavior to prefab

--- src/js/game.js
+++ src/js/game.js
@@ -2,82 +2,38 @@
   'use strict';
 
   function Game() {
-    this.player = null;
   }
 
   Game.prototype = {
 
     create: function () {
-      this.plane = new window['duster'].Plane(this.game, 100, 100)
-
-      this.ROTATION_SPEED = 110; // degrees/second
-      this.ROTATION_ACCELERATION = 110; // degrees/second
-      this.ACCELERATION = 600; // pixels/second/second
-      this.MAX_SPEED = 400; // pixels/second
-      this.DRAG = 50; // pixels/second
       this.GRAVITY = 200; // pixels/second/second
-      this.BOOST_VECTOR = 0.8 // rotation that acceleration is applied
 
       this.game.world.setBounds(0, 0, 5000, 600)
       this.game.add.tileSprite(0, 0, 5000, 600, 'bg')
       this.ground = this.game.add.tileSprite(0, this.game.height - 10, 99999, 10, 'player')
 
-      // Add the player to the stage
-      this.player = this.game.add.sprite(this.game.width/2, 100, 'player');
-      this.player.anchor.setTo(0.5, 0.5);
-      // this.player.angle = -90; // Point the player up
-      this.player.width = 50
-      this.player.height = 10
+      // Add the plane
+      this.plane = new window['duster'].Plane(this.game, 100, 100)
+      this.game.add.existing(this.plane)
 
       // Enable physics on
-      this.game.physics.arcade.enable(this.player)
       this.game.physics.arcade.enableBody(this.ground)
-      this.player.body.collideWorldBounds = true
       this.ground.body.immovable = true
       this.ground.body.allowGravity = false
-
-      // Set maximum velocity
-      this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED)
-
-      // Add drag to the player that slows it down when it is not accelerating
-      this.player.body.drag.setTo(this.DRAG, this.DRAG); // x, y
 
       // Turn on gravity
       this.game.physics.arcade.gravity.y = this.GRAVITY;
 
-      // Make player bounce a little
-      this.player.body.bounce.setTo(0.25, 0.25);
-
       // Input
       this.input.onDown.add(this.onInputDown, this);
 
-      this.game.camera.follow(this.player)
-
-      // Smoke
-      this.smokeEmitter = this.game.add.emitter(0, 0, 100)
-      this.smokeEmitter.makeParticles('smoke')
-      this.smokeEmitter.start(false, 4000, 20)
+      this.game.camera.follow(this.plane)
     },
 
     update: function () {
-      this.game.physics.arcade.collide(this.player, this.ground)
-      this.smokeEmitter.x = this.player.x
-      this.smokeEmitter.y = this.player.y
-      // Constrain velocity
-      // this.player.body.velocity.y = Math.sin(this.player.rotation) * this.player.body.speed
-      // this.player.body.velocity.x = Math.cos(this.player.rotation) * this.player.body.speed
-
-      if (this.input.activePointer.isDown) {
-        this.smokeEmitter.on = true
-        this.player.body.angularVelocity = -this.ROTATION_SPEED
-        this.player.body.acceleration.x = Math.cos(this.player.rotation - this.BOOST_VECTOR) * this.ACCELERATION;
-        this.player.body.acceleration.y = Math.sin(this.player.rotation - this.BOOST_VECTOR) * this.ACCELERATION;
-      } else {
-        this.smokeEmitter.on = false
-        this.player.body.acceleration.set(0)
-        this.player.body.angularVelocity = Math.cos(this.player.rotation + 0.5) * 100
-      }
-
+      this.game.physics.arcade.collide(this.plane, this.ground)
+      this.plane.isBoosting = this.input.activePointer.isDown
 
     },
 
@@ -89,7 +45,7 @@
       // this.game.debug.text('Vy: ' + this.player.body.velocity.y.toFixed(), 10, 40)
       // this.game.debug.text('Rotation: ' + this.player.rotation.toFixed(1), 10, 60)
       // this.game.debug.text('Speed: ' + this.player.body.speed.toFixed(), 10, 80)
-      this.game.debug.spriteCoords(this.player, 300, 500)
+      this.game.debug.spriteCoords(this.plane, 300, 500)
       this.game.debug.cameraInfo(this.game.camera, 32, 64)
     }
 
--- src/js/plane_prefab.js
+++ src/js/plane_prefab.js
@@ -3,14 +3,49 @@
 
   var Plane = function(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'player', 0)
-    console.log('plane made!')
+    this.game.physics.arcade.enable(this)
+    this.body.collideWorldBounds = true
+
+    this.anchor.setTo(0.5, 0.5)
+    this.width = 50
+    this.height = 10
+
+    this.ROTATION_SPEED = 110; // degrees/second
+    this.ROTATION_ACCELERATION = 110; // degrees/second
+    this.ACCELERATION = 600; // pixels/second/second
+    this.MAX_SPEED = 400; // pixels/second
+    this.DRAG = 50; // pixels/second
+    this.BOOST_VECTOR = 0.8 // rotation that acceleration is applied
+    // Set maximum velocity
+    this.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED)
+    this.body.drag.setTo(this.DRAG, this.DRAG); // x, y
+
+    // Smoke
+    this.smokeEmitter = this.game.add.emitter(0, 0, 100)
+    this.smokeEmitter.makeParticles('smoke')
+    this.smokeEmitter.start(false, 1000, 40)
+
+    this.isBoosting = false
   }
 
   Plane.prototype = Object.create(Phaser.Sprite.prototype)
   Plane.prototype.constructor = Plane
 
   Plane.prototype.update = function() {
+    this.smokeEmitter.x = this.x
+    this.smokeEmitter.y = this.y
+
+    if (this.isBoosting) {
+      this.body.angularVelocity = -this.ROTATION_SPEED
+      this.body.acceleration.x = Math.cos(this.rotation - this.BOOST_VECTOR) * this.ACCELERATION;
+      this.body.acceleration.y = Math.sin(this.rotation - this.BOOST_VECTOR) * this.ACCELERATION;
+
+    } else {
+      this.body.acceleration.set(0)
+      this.body.angularVelocity = Math.cos(this.rotation + 0.5) *100
+    }
   }
+
 
   window['duster'] = window['duster'] || {}
   window['duster'].Plane = Plane
